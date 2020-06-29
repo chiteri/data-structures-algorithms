31987669a20cd2a132c8d0269981195d4050425b
Martin Chiteri <martin.chiteri@gmail.com>
2020-06-29 15:43:07 -0600

    Alter variable names in the script

--- search-sort/binary_search.py
+++ search-sort/binary_search.py
@@ -1,25 +1,27 @@
 class BinarySearch:
     def __init__(self):
-        pass 
+        pass # Empty constructor
 
-    def search_helper(self, key, haystack):
-        return self.search(key, haystack, 0, len(haystack))
+    def search_helper(self, key, items):
+        return self.search(key, items, 0, len(items))
     
-    # Get the index of string key inside the search string array 
-    def search(self, key, search_string, low, high):
+    # Get the index of string key inside the search array 
+    def search(self, needle, haystack, low, high):
         if high <= low:
             return -1 
         
         middle = low + (high - low) /  2
 
-        if key < search_string[middle]:
+        # Perform a comparison of items in the search list / array 
+        # (lexicographic comparison in case of strings)
+        if needle < haystack[middle]:
             # Search in the lower half
-            return self.search(key, search_string, low, middle)
+            return self.search(needle, haystack, low, middle)
 
-        if key > search_string[middle]:
+        if needle > haystack[middle]:
             # Search in the upper half
-            return self.search(key, search_string, middle + 1, high)
+            return self.search(needle, haystack, middle + 1, high)
 
-        if key == search_string[middle]:
+        if needle == haystack[middle]:
             # Results found
             return middle
